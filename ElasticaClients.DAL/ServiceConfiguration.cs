0b9a1cd104565d936948261a9b5c92a9cce1e504
DENDUKA <49064635+DENDUKA@users.noreply.github.com>
2026-04-03 01:02:18 +0400

    1

--- ElasticaClients.DAL/ServiceConfiguration.cs
+++ ElasticaClients.DAL/ServiceConfiguration.cs
@@ -10,6 +10,15 @@ namespace ElasticaClients.DAL
         public static IServiceCollection AddDataAccessServices(this IServiceCollection services)
         {
             // Регистрация Mock сервисов (Singleton для сохранения состояния в памяти)
+            //services.AddSingleton<IAccountDAL, MockAccountDAL>();
+            //services.AddSingleton<IBranchDAL, MockBranchDAL>();
+            //services.AddSingleton<IGymDAL, MockGymDAL>();
+            //services.AddSingleton<IIncomeDAL, MockIncomeDAL>();
+            //services.AddSingleton<IRoleDAL, MockRoleDAL>();
+            //services.AddSingleton<ISubscriptionDAL, MockSubscriptionDAL>();
+            //services.AddSingleton<ITrainingDAL, MockTrainingDAL>();
+            //services.AddSingleton<ITrainingItemDAL, MockTrainingItemDAL>();
+
             services.AddSingleton<IAccountDAL, AccountDAL>();
             services.AddSingleton<IBranchDAL, BranchDAL>();
             services.AddSingleton<IGymDAL, GymDAL>();
